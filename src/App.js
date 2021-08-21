35bed272344b6b7ebf75844575b926a8243da403
erinjaym <erin.jay.moyer@gmail.com>
2021-08-20 20:47:55 -0400

    Created StartScreen component. Created basic styles of start screen based on concepts written out. Created wanted posters in photoshop to use on startscreen. Added an Arcade font to fit app scheme. Next Steps: Add Router Dom to App and create a GameBoard component that user will be routed to after start button is selected.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import './fonts/arcade-regular.ttf';
+import StartScreen from "./StartScreen";
+import SF from './images/sfpic.png';
+import GameBoard from './GameBoard';
 
 function App() {
   return (
     <div className="">
-      
+      <StartScreen /> 
     </div>
   );
 }
