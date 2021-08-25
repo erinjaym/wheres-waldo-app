be8bdaf0fb3edffdd50a3f3b03e627f1ea19805c
erinjaym <erin.jay.moyer@gmail.com>
2021-08-25 15:06:03 -0400

    Added a clipboard icon to GameLegend that will now display in upper right hand of screen on gameboard. Created functions to allow clicks on clipboard icon to show and hide legend. Created css animation for legend showing and hiding. Stylized font and list in clipboard. changed color of gameBoard border to match gameLegend Styling. Temporarily increased gameBoard border size. May keep as it looks better and gives more of a gamey appearance. Next Steps: Style show and hide links in game legend and create functions to show and hide character images in legend when clicked.

--- src/components/GameLegend.js
+++ src/components/GameLegend.js
@@ -7,31 +7,59 @@ import { useState, useEffect } from "react";
 
 const GameLegend = () => {
 
+    const [legendDisplay, setLegendDisplay] = useState(false);
+
+    useEffect(() => {
+        const clipBoard = document.getElementById("legend-icon");
+        clipBoard.addEventListener("click", showOrHideLegend);
+        return function cleanup (){
+            clipBoard.removeEventListener("click", showOrHideLegend);
+        }
+    });
+
+        // need to change cursor when hovering above clipboard for gamelegend
     function showHints () {
         // let hints = document.getElementsById("hint-pic"); < ... change to a hook ??
         // hints.style.display = "inline-block";
       }
 
-return ( 
-<div id="legend" className="legend">
-    <img src={Assignment} />
+    const showOrHideLegend = () => {
+        if (legendDisplay == true){
+            document.getElementById("legendWrapper").className = "legend-hidden";
+            document.getElementById("legendList").style.display = "none";
+            setLegendDisplay(false);
+        }else{
+            document.getElementById("legendWrapper").className = "legend-shown";
+            document.getElementById("legendList").style.display = "flex";
+            setLegendDisplay(true);
+        }
+    }
 
-    <p style={ {textAlign: "center"} }> Who you need to hunt!</p>
-    <li>
-    Samus   <br /> 
-   <div id="hint1"><img id="hint-pic" className="hint-pic" src={SamusHint} /> </div>
-    </li>
-    <li>
-    Vault Boy <br />
-    <div id="hint2"><img id="hint-pic" className="hint-pic" src={VaultBoyHint} /> </div>
-    </li>
-    <li>
-    Boba Fett<br />
-    <div id="hint3"><img id="hint-pic" className="hint-pic" src={BobaHint} /> </div>
-    </li><br />
-    <li>
-    <a href="#" onClick={() => showHints()}>SHOW HINTS</a> <a href="#" >HIDE HINTS</a>
-    </li>
+return ( 
+<div id="legend" >
+    <img id="legend-icon" className="legend-icon" alt="clipboard" src={Assignment} />
+    <div id="legendWrapper" className="legend-hidden">
+    <div id="legendList" className="legendList" >
+        <p style={ {textAlign: "center"} }> Who you need to hunt!</p>
+        <li>
+        Samus   <br /> 
+    <div id="hint1"><img id="hint-pic" className="hint-pic" src={SamusHint} /> </div>
+        </li>
+        <li>
+        Vault Boy <br />
+        <div id="hint2"><img id="hint-pic" className="hint-pic" src={VaultBoyHint} /> </div>
+        </li>
+        <li>
+        Boba Fett<br />
+        <div id="hint3"><img id="hint-pic" className="hint-pic" src={BobaHint} /> </div>
+        </li><br />
+        <li>
+        <a href="#" onClick={() => showHints()}>SHOW HINTS</a> <a href="#" >HIDE HINTS</a>
+        </li>
+    </div>
+    </div>
+    
+
 </div>
 );
 }
